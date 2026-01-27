50c29d81aacee68b06f2fc3c8d6f1c22cf2906fc
Yongkang <zeenny.willians@sjtu.edu.cn>
2026-01-27 16:59:33 +0800

    kv cache

--- temp/run.sh
+++ temp/run.sh
@@ -26,13 +26,13 @@ cmake --build build --config Release
 # 根据模式选择运行哪个版本
 if [ "$EAGLE_MODE" = true ]; then
     echo "运行Eagle版本..."
-    CUDA_VISIBLE_DEVICES=0 \
+    CUDA_VISIBLE_DEVICES=2 \
     build/bin/llama-simple -m $MODEL_PATH \
         -n 256 "Once upon a time, there is a big tree" \
         >temp/out.txt 2>temp/out.err
 else
     echo "运行标准版本..."
-    CUDA_VISIBLE_DEVICES=0 \
+    CUDA_VISIBLE_DEVICES=2 \
     build/bin/llama-simple -m $MODEL_PATH \
         -n 256 "Once upon a time, there is a big tree"
 fi
